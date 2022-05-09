02014e666b1dced05ef8c4c03b1988b620dacb07
Osman <osmango9697@gmail.com>
2022-05-08 21:48:03 -0300

    Consigna 7: Formulario

--- src/routes/productosRuta.js
+++ src/routes/productosRuta.js
@@ -4,5 +4,6 @@ const productosControllers = require("../controllers/productosControllers");
 
 router.get("/carrito", productosControllers.carrito); 
 router.get("/ficha", productosControllers.detalle);   
+router.get("/abm", productosControllers.crear);   
 
 module.exports = router;
