19e5f5a4907cce03020d5d3ddba395ee853bd3b7
Osman <osmango9697@gmail.com>
2022-05-22 19:27:58 -0300

    listo

--- src/controllers/productosControllers.js
+++ src/controllers/productosControllers.js
@@ -1,13 +1,29 @@
 const fs = require ("fs");
 const path = require ("path");
-const products= JSON.stringify("../data/products.json")
+const productsFilePath =  path.join(__dirname, "../data/products.json")
+const productsJson = fs.readFileSync(productsFilePath, "utf-8")
+const products= JSON.parse(productsJson)
 const controller = {
     carrito:  (req, res) =>{res.render(path.join(__dirname,"../views/products/carritoCompra"))},
     detalle: (req, res) =>{res.render(path.join(__dirname,"../views/products/productDetail"))},
     crear: (req, res) =>{res.render(path.join(__dirname,"../views/products/creacionProducto"))},
     crearProducto: (req, res)=>{
-        req.body.push(products)
-        fs.writeFileSync(products)
+        let image;
+        if(req.file != undefined){
+            image = req.file.filename
+        } else {
+            image = 'image-default.jpg'
+        }
+        let newProduct = {
+            id: products[products.length - 1].id + 1,
+            
+                ...req.body
+            ,
+            imagen: image
+        };
+        products.push(newProduct)
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '))
+        res.redirect('/');
     },
 }
 
