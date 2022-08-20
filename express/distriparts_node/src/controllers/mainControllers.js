186fdc4fd4d9f2a2a71443cc6316793396e29e93
Bruno Dalberto <bdalber15@gmail.com>
2022-08-20 12:42:38 -0300

    detalles

--- express/distriparts_node/src/controllers/mainControllers.js
+++ express/distriparts_node/src/controllers/mainControllers.js
@@ -3,8 +3,22 @@ const fs = require ("fs");
 const productsFilePath = path.join(__dirname, "../data/products.json")
 const productsJson = fs.readFileSync(productsFilePath, "utf-8")
 const products= JSON.parse(productsJson)
+const db = require("../database/models")
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
 
 module.exports = {
-    index: (req,res)=> { res.render(path.join(__dirname,"../views/users/index"), {products:products})},
+    index: (req,res)=>  { res.render(path.join(__dirname,"../views/users/index"), {products:products})},
+
+    search:(req, res) => {
+        const {term} = req.query;
+
+        db.Product.findAll({
+            where:{nombre:{[Op.like]:'%' + term + '%'}},
+            include:["sections"]
+          })
+          .then(products=> res.render("./products", {products:products}))
+    }
 }
+
 
